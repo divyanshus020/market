7c3623f147fd1872004e0fb97cc7f9b2583ac275
divyanshus020 <sharmadivyanshu281@gmail.com>
2023-10-29 14:59:49 +0530

    adding assets in src

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Navbar from "./components/navbar.jsx";
-
+import Insure from "./components/insure.jsx";
 function App() {
   return (
     <>
       <Navbar />
+      <Insure/>
     </>
   );
 }
---
+++ src/components/insure.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+
+function Insure() {
+    return (
+        <div className='landing_page'>
+
+            {/* first bar */}
+            <div className='' id='landing_1'>
+                <div className=''>
+                    <img src="" alt="" />
+                    <h3>Two-wheeler insurence</h3>
+                </div>
+                <div className=''>
+                    <img src="" alt="" />
+                    <h3>Car insurence</h3>
+                </div>
+                <div className=''>
+                    <img src="" alt="" />
+                    <h3>Health insurence</h3>
+                </div>
+                <div className=''>
+                    <img src="" alt="" />
+                    <h3>Hospicash</h3>
+                </div>
+                <div className=''>
+                    <img src="" alt="" />
+                    <h3>Life insurence</h3>
+                </div>
+            </div>
+
+            {/* second bar */}
+            <div className='' id='landing_2'>
+
+                <div className=''>
+                    <img src="" alt="" />
+                    <h3>InsureRight</h3>
+                </div>
+
+                <div className=''>
+                    <img src="" alt="" />
+                    <h3>InsureFit 360</h3>
+                </div>
+
+                <div className=''>
+                    <img src="" alt="" />
+                    <h3>Shopkeeper Insurence</h3>
+                </div>
+
+                <div className=''>
+                    <img src="" alt="" />
+                    <h3>Savings & Insurence</h3>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Insure
