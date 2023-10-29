b826cb48d98c481d98110f40bddecb39f97969e2
divyanshus020 <sharmadivyanshu281@gmail.com>
2023-10-29 11:52:38 +0530

    carouselpage but not connected

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/navbar.jsx'
 
-export const App =() =>{
-return <Navbar />;
+
+function App() {
+    return (
+        <div className='App'>
+            <Navbar />
+        </div>
+    );
 };
 
 export default App;
