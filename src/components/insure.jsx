24753ce0464625652f8873c58190b8ecc45f0af9
divyanshus020 <sharmadivyanshu281@gmail.com>
2023-10-31 00:38:32 +0530

    footer and alert bar is added

--- src/components/insure.jsx
+++ src/components/insure.jsx
@@ -90,49 +90,48 @@ function Insure() {
         </div>
       </div>
 
-      <section id="home" className="py-5 relative top-[180px]" >
+      <section id="home" className="py-5 relative top-[10px]" >
 
         <div className="container_1 flex flex-wrap items-center justify-around mx-auto mt-10 md:px-12 md:flex-row">
           <div className="mb-14 lg:mb-0 lg:w-2/2 w-[500px]">
-            
-              <h1 className='text-[40px] text-[#465a65]  font-semibold'><span className='underline decoration-[#e27e28]'>Insure</span> Right</h1>
 
-              <p className='text-[24px] text-[#707070] mb-[35px]'>Let our proprietary tool suggest<br></br>
-                the right coverage for you in under 4 mins
-              </p>
+            <h1 className='text-[40px] text-[#465a65]  font-semibold'><span className='underline decoration-[#e27e28]'>Insure</span> Right</h1>
+
+            <p className='text-[24px] text-[#707070] mb-[35px]'>Let our proprietary tool suggest<br></br>
+              the right coverage for you in under 4 mins
+            </p>
 
 
-              <button className="bg-[#e27e28] hover:bg-blue-700 text-white font-bold py-5 px-12 rounded">
-                <h2 className='text-[15px]'>Enroll now</h2>
-              </button>
+            <button className="bg-[#e27e28] hover:bg-blue-700 text-white font-bold py-5 px-12 rounded">
+              <h2 className='text-[15px]'>Enroll now</h2>
+            </button>
 
-            </div>
+          </div>
 
-            <div className=''>
-              <div className='lg:w-2/2'>
-                <img src="https://finizoninsurance.com/assets/img/protect_right_illustration.svg" alt="" />
-              </div>
+          <div className=''>
+            <div className='lg:w-2/2'>
+              <img src="https://finizoninsurance.com/assets/img/protect_right_illustration.svg" alt="" />
             </div>
           </div>
+        </div>
       </section>
 
 
       {/*second hero section 2  */}
 
-      <section id="home" className="py-5 relative top-[180px]" >
+      <section id="home" className="py-5 relative top-[100px]" >
 
         <div className="container_1 flex flex-wrap items-center justify-around mx-auto mt-10 md:px-12 md:flex-row">
 
 
           <div className=" mb-14 lg:mb-0 lg:w-2/2 w-[500px]">
 
-            <h1 className="max-w-full text-[2.9rem] leading-none text-gray-900 font-bold font-sans text-center lg:text-4xl lg:text-center lg:leading-tight mb-7">
-              Digital Tools to personify your needs
+            <h1 className="max-w-full text-[2.9rem] leading-none text-gray-900 font-bold font-sans text-center lg:text-5xl lg:text-center lg:leading-tight mb-7">
+              Compare Insurance across Companies
             </h1>
 
-            <p className="max-w-full text-center text-gray-500 lg:text-right lg:max-w-4xl text-[20px]">
-              The platform gives much more than jst insurance. Use our proprietary tools to find the right
-              protection solutions, wellness programs, emergency assistance programs etc.
+            <p className="max-w-full text-center text-gray-500 lg:text-center lg:max-w-4xl text-[20px]">
+              Be it Life, Motor or Health insurance, compare insurance plans across 35+ insurers
             </p>
           </div>
 
@@ -155,13 +154,13 @@ function Insure() {
 
           <div className=" mb-14 lg:mb-0 lg:w-2/2 w-[500px]">
 
-            <h1 className="max-w-full text-[2.9rem] leading-none text-gray-900 font-bold font-sans text-center lg:text-4xl lg:text-center lg:leading-tight mb-7">
-              Digital Tools to personify your needs
+            <h1 className="max-w-full text-[2.9rem] leading-none text-gray-900 font-bold font-sans text-center lg:text-5xl lg:text-center lg:leading-tight mb-7">
+              Manage all your Policies at One Place
             </h1>
 
-            <p className="max-w-full text-center text-gray-500 lg:text-right lg:max-w-4xl text-[20px]">
-              The platform gives much more than jst insurance. Use our proprietary tools to find the right
-              protection solutions, wellness programs, emergency assistance programs etc.
+            <p className="max-w-full text-center text-gray-500 lg:text-center lg:max-w-4xl text-[20px]">
+              Have all your policies handy in one place when you need them at the time
+              of claim; or renewal
             </p>
           </div>
 
@@ -176,8 +175,8 @@ function Insure() {
 
           <div className=" mb-14 lg:mb-0 lg:w-2/2 w-[500px]">
 
-            <h1 className="max-w-full text-[2.9rem] leading-none text-gray-900 font-bold font-sans text-center lg:text-4xl lg:text-center lg:leading-tight mb-7">
-              Digital Tools to personify your needs
+            <h1 className="max-w-full text-[2.9rem] leading-none text-gray-900 font-bold font-sans text-center lg:text-5xl lg:text-center lg:leading-tight mb-7">
+              Assistance On-the-Go
             </h1>
 
             <p className="max-w-full text-center text-gray-500 lg:text-right lg:max-w-4xl text-[20px]">
@@ -208,7 +207,7 @@ function Insure() {
 
           <div className=" mb-14 lg:mb-0 lg:w-2/2 w-[500px]">
 
-            <h1 className="max-w-full text-[2.9rem] leading-none text-gray-900 font-bold font-sans text-center lg:text-4xl lg:text-center lg:leading-tight mb-7">
+            <h1 className="max-w-full text-[2.9rem] leading-none text-gray-900 font-bold font-sans text-center lg:text-5xl lg:text-center lg:leading-tight mb-7">
               Digital Tools to personify your needs
             </h1>
 
@@ -219,53 +218,164 @@ function Insure() {
 
           </div>
         </div>
+      </section>
 
-        {/*lunching landing page */}
-        <section id="home" className="py-5">
+      {/*lunching landing page */}
+      <section id="home" className="py-5 relative top-[190px]">
 
-          <section className='pcontainer_1 flex flex-wrap items-center justify-evenly mx-auto mt-10 md:px-12 md:flex-row'>
+        <section className='pcontainer_1 flex flex-wrap items-center justify-evenly mx-auto mt-10 md:px-12 md:flex-row'>
 
-            <div className='lg:w-2/2'>
-              <img src="https://finizoninsurance.com/assets/img/Partnership-bro.svg" alt="" />
-            </div>
+          <div className='lg:w-2/2'>
+            <img src="https://finizoninsurance.com/assets/img/Partnership-bro.svg" alt="" />
+          </div>
 
 
-            <div className=" mb-17 lg:mb-0 lg:w-2/2 w-[500px] ">
-              <h1 className="max-w-full text-[2.9rem] leading-none text-gray-900 font-bold font-sans lg:text-5xl lg:leading-tight mb-7">
-                <br />
-                “Launching India’s first
-                <br />
-                <span className='text-[#00a0ad]'>phydigital finance experience!”</span>
-              </h1>
+          <div className=" mb-17 lg:mb-0 lg:w-2/2 w-[500px] ">
+            <h1 className="max-w-full text-[2.9rem] leading-none text-gray-900 font-bold font-sans lg:text-5xl lg:leading-tight mb-7">
+              <br />
+              “Launching India’s first
+              <br />
+              <span className='text-[#00a0ad]'>phydigital finance experience!”</span>
+            </h1>
 
-              <div className="max-w-full text-gray-500 text-[20px] ml-10">
+            <div className="max-w-full text-gray-500 text-[20px] ml-10">
 
-                <div className='flex gap-2'>
-                  <img src="https://finizoninsurance.com/assets/img/right-arrow.png" alt="" />
-                  <p>Make decisions closely with the experts</p>
-                </div>
-                <br />
-                <div className='flex gap-2'>
-                  <img src="https://finizoninsurance.com/assets/img/right-arrow.png" alt="" />
-                  <p>Find the right financial products and tools for you</p>
-                </div>
-                <br />
-                <div className='flex gap-2'>
-                  <img src="https://finizoninsurance.com/assets/img/right-arrow.png" alt="" />
-                  <p>Never miss out on the best available offers</p>
-                </div>
-
+              <div className='flex gap-2'>
+                <img src="https://finizoninsurance.com/assets/img/right-arrow.png" alt="" />
+                <p>Make decisions closely with the experts</p>
+              </div>
+              <br />
+              <div className='flex gap-2'>
+                <img src="https://finizoninsurance.com/assets/img/right-arrow.png" alt="" />
+                <p>Find the right financial products and tools for you</p>
+              </div>
+              <br />
+              <div className='flex gap-2'>
+                <img src="https://finizoninsurance.com/assets/img/right-arrow.png" alt="" />
+                <p>Never miss out on the best available offers</p>
               </div>
 
-              <div className="flex justify-center mt-14 lg:justify-start">
-                <button type="button" className="text-white bg-[#e27e28] font-medium rounded-lg px-20 py-7 text-center hover:bg-indigo-500 hover:drop-shadow-md transition duration-300 ease-in-out">
-                  learn more
-                </button>
-              </div>
             </div>
-          </section>
+
+            <div className="flex justify-center mt-14 lg:justify-start">
+              <button type="button" className="text-white bg-[#e27e28] font-medium rounded-lg px-20 py-7 text-center hover:bg-indigo-500 hover:drop-shadow-md transition duration-300 ease-in-out">
+                learn more
+              </button>
+            </div>
+          </div>
         </section>
       </section>
+
+      {/* same for all alert page */}
+
+      <section className="py-5 relative top-[250px] flex justify-center">
+        <div className="flex justify-evenly rounded-3xl bg-[#fff] z-1 alert_1 xl:w-[80%]">
+
+          <div>
+            <img src="https://finizon.com/assets/img/grow_investment_icon.svg" alt="" />
+          </div>
+
+          <div className="text-[42px] text-bold">
+            <h1>Grow your family's investment in<br></br>one place</h1>
+          </div>
+
+          <div className="flex flex-col justify-center">
+            <button type="button" className="text-white bg-[#e27e28] font-medium rounded-lg px-20 py-7 text-center hover:bg-indigo-500 hover:drop-shadow-md transition duration-300 ease-in-out text-[12px]">
+              Get Started
+            </button>
+          </div>
+        </div>
+      </section>
+
+      {/* footer section */}
+
+
+      <footer className=" dark:bg-gray-900 relative top-[1900px]">
+        <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+          <div className="md:flex md:justify-between">
+            <div className="mb-6 md:mb-0">
+              <a href="https://flowbite.com/" className="flex items-center">
+                <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="FlowBite Logo" />
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">MarketMagent</span>
+              </a>
+            </div>
+            <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+              <div>
+                <h2 className="mb-6 text-[20px] font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
+                <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                  <li className="mb-4">
+                    <a href="https://flowbite.com/" className="hover:underline text-[18px]">Flowbite</a>
+                  </li>
+                  <li>
+                    <a href="https://tailwindcss.com/" className="hover:underline text-[18px]">Tailwind CSS</a>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <h2 className="mb-6 text-[20px] font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
+                <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                  <li className="mb-4">
+                    <a href="https://github.com/themesberg/flowbite" className="hover:underline text-[18px]">Github</a>
+                  </li>
+                  <li>
+                    <a href="https://discord.gg/4eeurUVvTy" className="hover:underline text-[18px]">Discord</a>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <h2 className="mb-6 text-[20px] font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
+                <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                  <li className="mb-4">
+                    <a href="#" className="hover:underline text-[18px]">Privacy Policy</a>
+                  </li>
+                  <li>
+                    <a href="#" className="hover:underline text-[18px]">Terms &amp; Conditions</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+          <div className="sm:flex sm:items-center sm:justify-between">
+            <span className="text-[12px] text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline ">MarketMagnet™</a>. All Rights Reserved.
+            </span>
+
+            <div className="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
+              <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
+                  <path fill-rule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clip-rule="evenodd" />
+                </svg>
+                <span className="sr-only">Facebook page</span>
+              </a>
+              <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 21 16">
+                  <path d="M16.942 1.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.585 11.585 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3A17.392 17.392 0 0 0 .182 13.218a15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.63 10.63 0 0 1-1.706-.83c.143-.106.283-.217.418-.33a11.664 11.664 0 0 0 10.118 0c.137.113.277.224.418.33-.544.328-1.116.606-1.71.832a12.52 12.52 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM6.678 10.813a1.941 1.941 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z" />
+                </svg>
+                <span className="sr-only">Discord community</span>
+              </a>
+              <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 17">
+                  <path fill-rule="evenodd" d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z" clip-rule="evenodd" />
+                </svg>
+                <span className="sr-only">Twitter page</span>
+              </a>
+              <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                  <path fill-rule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clip-rule="evenodd" />
+                </svg>
+                <span className="sr-only">GitHub account</span>
+              </a>
+              <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                  <path fill-rule="evenodd" d="M10 0a10 10 0 1 0 10 10A10.009 10.009 0 0 0 10 0Zm6.613 4.614a8.523 8.523 0 0 1 1.93 5.32 20.094 20.094 0 0 0-5.949-.274c-.059-.149-.122-.292-.184-.441a23.879 23.879 0 0 0-.566-1.239 11.41 11.41 0 0 0 4.769-3.366ZM8 1.707a8.821 8.821 0 0 1 2-.238 8.5 8.5 0 0 1 5.664 2.152 9.608 9.608 0 0 1-4.476 3.087A45.758 45.758 0 0 0 8 1.707ZM1.642 8.262a8.57 8.57 0 0 1 4.73-5.981A53.998 53.998 0 0 1 9.54 7.222a32.078 32.078 0 0 1-7.9 1.04h.002Zm2.01 7.46a8.51 8.51 0 0 1-2.2-5.707v-.262a31.64 31.64 0 0 0 8.777-1.219c.243.477.477.964.692 1.449-.114.032-.227.067-.336.1a13.569 13.569 0 0 0-6.942 5.636l.009.003ZM10 18.556a8.508 8.508 0 0 1-5.243-1.8 11.717 11.717 0 0 1 6.7-5.332.509.509 0 0 1 .055-.02 35.65 35.65 0 0 1 1.819 6.476 8.476 8.476 0 0 1-3.331.676Zm4.772-1.462A37.232 37.232 0 0 0 13.113 11a12.513 12.513 0 0 1 5.321.364 8.56 8.56 0 0 1-3.66 5.73h-.002Z" clip-rule="evenodd" />
+                </svg>
+                <span className="sr-only">Dribbble account</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </footer>
+
     </section>
   );
 }
