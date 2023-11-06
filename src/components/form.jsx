c2f70ee8137709e865e7b1095e3b61a23fffcb12
divyanshus020 <sharmadivyanshu281@gmail.com>
2023-11-06 20:55:03 +0530

    form without validation

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,22 +1,12 @@
 import React from 'react'
 
 function Form() {
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbwplo5oPMUve-niq8qicZ8u2cm-RNowypcD-AnXCOrQmcXiG6uJGgV11YMtEvTO4r71Ng/exec'
-    const form = document.forms['submit-to-google-sheet']
 
-    form.addEventListener('submit', e => {
-      e.preventDefault()
-      fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-        .then(response => console.log('Success!', response))
-        .catch(error => console.error('Error!', error.message))
-    })
 
-    
     return (
-        <div class="max-w-md mx-auto bg-white p-8 rounded-3xl shadow-lg">
+        <div class=" max-w-md mx-auto bg-white p-8 rounded-3xl shadow-lg">
             <h2 class="text-2xl font-semibold mb-4">Please fill the form below</h2>
-
-            <form name='submit-to-google-sheet' >
+            <form method='post' action='https://script.google.com/macros/s/AKfycbw3-OzsnbdjpUqruWIww6BJZ24Ov1Vu7gfZz-69R-KyKKFqHe43TCupqFYXw_U8ac4TQg/exec' >
                 <div class="mb-4">
                     <label for="name" class="block text-gray-600">Name</label>
                     <input type="text" id="name" name="name" class="w-full p-2 border rounded focus:outline-none focus:border-blue-400" placeholder="John Doe" />
